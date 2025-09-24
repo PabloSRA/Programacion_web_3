07b0cbe90c45f89a82db8e269b81335f36984076
PabloSRA <projasa@fcpn.edu.bo>
2025-09-24 01:54:07 -0400

    Delete Programacion web 3/Practica_1 directory

--- Programacion web 3/Practica_1/ejercicio6/index.js
+++
@@ -1,4 +0,0 @@
-const vector = [1,2,3,4,5,6];
-const [a,b] = vector;
-
-console.log(a,b);
--- Programacion web 3/Practica_1/ejercicio9/index.js
+++
@@ -1,5 +0,0 @@
-const promesa = new Promise(()=>{
-    setTimeout(()=>{
-        console.log("el progrma se ejecuto en 3 segundos");
-    },3000);
-})
